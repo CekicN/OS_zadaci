c939902dff282091642d331566c8455364f7d6c7
osboxes.org <cekicnikola53@gmail.com>
2022-12-10 01:15:05 +0100

    komit

--- April_2022_1.c
+++ April_2022_1.c
@@ -1,31 +1,52 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<unistd.h>
+#include<semaphore.h>
+
+sem_t glavni;
+sem_t func;
+
+int broj;
 void* Broji(void *arg)
 {
-    int broj = *((int *)arg);
-    for (int i = 0; i < broj; i++)
+    while (1)
     {
-        printf("%d\n", i);
-        sleep(3);
+        sem_wait(&func);
+        if(broj==99)
+            break;
+        for (int i = 0; i < broj; i++)
+        {
+            printf("%d\n", i);
+            sleep(3);
+        }
+        sem_post(&glavni);
     }
+    
+    
     
 }
 int main(int argc, char* argv[])
 {
     pthread_t nit;
-    int broj;
+    sem_init(&func,0,0);
+    sem_init(&glavni,0,0);
+    pthread_create(&nit, NULL, &Broji,NULL);
+  
     printf("Unesi broj:");
     scanf("%d", &broj);
-
     while (broj != 99)
     {
-        pthread_create(&nit,NULL,&Broji,&broj);
-        pthread_join(nit,NULL);
-        pthread_cancel(nit);
+        sem_post(&func);
+        sem_wait(&glavni);
+
         printf("Unesi broj:");
         scanf("%d", &broj);
     }
-    
+    sem_post(&func);
+
+    pthread_join(nit, NULL);
+
+    sem_destroy(&glavni);
+    sem_destroy(&func);
     return 0;
 }
